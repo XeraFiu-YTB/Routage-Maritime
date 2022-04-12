3162d3454b39eee63dfc8726989dc781bf0f35c9
Nathan Bastard <nathan.bastardpfeg@gmail.com>
2022-04-13 00:53:43 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,31 +15,55 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let startingPoint = { "lat": 0, "lng": 0, "maker": null }
 let endingPoint = { "lat": 0, "lng": 0, "maker": null }
 
-//Listen Left & Right Click from the map
-;["click", "contextmenu"].forEach(function (eventName) {
-    map.on(eventName, (event) => {
-        switch(event.originalEvent.which) {
-            //Left Click
-            case 1:
-                if(startingPoint.maker) map.removeLayer(startingPoint.maker)
-                startingPoint = { "lat": event.latlng.lat, "lng": event.latlng.lng }
-                startingPoint.maker = L.marker([startingPoint.lat, startingPoint.lng], { alt: "Start" }).addTo(map)
-                startingPoint.maker.bindPopup("<b>Start</b>").openPopup()
-                updatePath(startingPoint, endingPoint)
-                //draw()
-            break;
-            //Right Click
-            case 3:
+window.onload = () => {
+    //Listen click to the map
+    if(isMobile()) {
+        let isStartPoint = false
+        map.on("click", (event) => {
+            console.log("click")
+            isStartPoint = !isStartPoint
+            if(isStartPoint) {
+                    if(startingPoint.maker) map.removeLayer(startingPoint.maker)
+                    startingPoint = { "lat": event.latlng.lat, "lng": event.latlng.lng }
+                    startingPoint.maker = L.marker([startingPoint.lat, startingPoint.lng], { alt: "Start" }).addTo(map)
+                    startingPoint.maker.bindPopup("<b>Start</b>").openPopup()
+                    updatePath(startingPoint, endingPoint)
+            } else {
                 if(endingPoint.maker) map.removeLayer(endingPoint.maker)
                 endingPoint = { "lat": event.latlng.lat, "lng": event.latlng.lng }
                 endingPoint.maker = L.marker([endingPoint.lat, endingPoint.lng], { alt: "End" }).addTo(map)
                 endingPoint.maker.bindPopup("<b>End</b>").openPopup()
                 updatePath(startingPoint, endingPoint)
-                //draw()
-            break;
-        }
-    })
-})
+            }
+        })
+    } else {
+        //Listen Left & Right Click from the map
+        ;["click", "contextmenu"].forEach(function (eventName) {
+            map.on(eventName, (event) => {
+                switch(event.originalEvent.which) {
+                    //Left Click
+                    case 1:
+                        if(startingPoint.maker) map.removeLayer(startingPoint.maker)
+                        startingPoint = { "lat": event.latlng.lat, "lng": event.latlng.lng }
+                        startingPoint.maker = L.marker([startingPoint.lat, startingPoint.lng], { alt: "Start" }).addTo(map)
+                        startingPoint.maker.bindPopup("<b>Start</b>").openPopup()
+                        updatePath(startingPoint, endingPoint)
+
+                    break;
+                    //Right Click
+                    case 3:
+                        if(endingPoint.maker) map.removeLayer(endingPoint.maker)
+                        endingPoint = { "lat": event.latlng.lat, "lng": event.latlng.lng }
+                        endingPoint.maker = L.marker([endingPoint.lat, endingPoint.lng], { alt: "End" }).addTo(map)
+                        endingPoint.maker.bindPopup("<b>End</b>").openPopup()
+                        updatePath(startingPoint, endingPoint)
+
+                    break;
+                }
+            })
+        })
+    }
+}
 
 //Display every path on the map
 let active = false
@@ -60,3 +84,18 @@ document.getElementById("show").onclick = () => {
 }
 
 
+function isMobile() {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ]
+    
+    return toMatch.some((toMatchItem) => {
+        return navigator.userAgent.match(toMatchItem)
+    })
+}
